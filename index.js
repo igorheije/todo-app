9136253e1075343fb3ac9939979a99822a103ed8
igorheije <heije.tomori2014@outlook.com>
2020-12-16 10:36:42 -0300

    implementando

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
+const valor = document.querySelector('#input')
+const buttons = document.getElementsByClassName('excluir')
+const numeros = document.getElementById('numberItens')
+// const icon = src('./images/icon-cross.svg')
+// console.log(icon);
+
 function criarTodo() {
-    var novaLi = document.createElement('li');
-    var textnode = document.createTextNode("Water")
-    novaLi.appendChild(textnode)
-    document.getElementById("itens").appendChild(novaLi)
+  if(valor.value==="")return null
+
+  var novaLi = document.createElement('li');
+  var circle = document.createElement('div')
+  var button = document.createElement('button')
+  var span = document.createElement('span')
+  
+  button.classList.add('excluir')
+  circle.classList.add('circle');
+  
+  var textnode = document.createTextNode(valor.value)
+  const excluir = document.createTextNode('X')
+
+  button.appendChild(excluir)
+  span.appendChild(textnode)
+  button.onclick = excluirTodo
+  
+  novaLi.appendChild(circle)
+  novaLi.appendChild(span)
+  novaLi.appendChild(button)
+
+  document.getElementById("itens").appendChild(novaLi)
+  quantidadeItem()
+  valor.value = ""
+  valor.focus()
   }
 
+function excluirTodo(event) {
+    event.path[1].remove() 
+    quantidadeItem()
+  }
+
+function quantidadeItem(){    
+    numeros.innerHTML = `${buttons.length} itens left`
+}
+
+function ativos(){
+  console.log('eu');
+}
+
+function verTodos(){
+  console.log('todos');
+}
+
+function completas(){
+  console.log('completas');
+}
